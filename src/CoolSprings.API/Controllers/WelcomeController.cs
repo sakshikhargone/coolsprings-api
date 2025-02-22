b4b7953a8dae1800375102391acd3d228b562dd0
sakshikhargone <sakshi.khargone@gmail.com>
2025-02-22 17:21:19 +0530

    dotnet 5 web api project setup

---
+++ src/CoolSprings.API/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoolSprings.API.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Welcome to CoolSprings API" });
+        }
+    }
+}
