526f3313d05e75b9837737a855d8512e7aa1f4c5
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-04 23:25:28 +0530

    Simplify booking entity structure

---
+++ src/CoolSprings.Model/Booking.cs
@@ -0,0 +1,19 @@
+﻿namespace CoolSprings.Model;
+
+public class Booking
+{
+    public Guid BookingId { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public Guid CustomerId { get; set; }
+    public string DiscountValue { get; set; }
+    public Guid? DiscountId { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public int NumOfTickets { get; set; }
+    public string PaymentMode { get; set; }
+    public float ActualAmount { get; set; }
+    public float PaidAmount { get; set; }
+    public string? UPITxnRefNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
