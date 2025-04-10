72628c521e1f3f9a4a7c64ea802f6fa72fd43185
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-10 23:16:15 +0530

    Implement EncodingService for serializing objects into plain text tokens

---
+++ src/CoolSprings.API/Services/EncodingService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace CoolSprings.API.Services
+{
+    public interface IEncoding
+    {
+         string Encoding(TokenCredential crendial);
+    }
+    
+    public class EncodingService : IEncoding
+    {
+        
+        public  string Encoding([FromBody]TokenCredential credential)
+        {
+            var token = JsonConvert.SerializeObject(credential);
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(token);
+            return System.Convert.ToBase64String(plainTextBytes);
+
+        }
+    }
+}
