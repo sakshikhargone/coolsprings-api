a0cca4ff62ad4e6a8bf7bae7b7dc4d0c7dc5d1d7
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-20 23:47:34 +0530

    Add GlobalUsings.cs file to centralize commonly used namespaces

---
+++ src/CoolSprings.API/GlobalUsing.cs
@@ -0,0 +1,21 @@
+﻿global using CoolSprings.Model;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using System;
+global using CoolSprings.Contract;
+global using CoolSprings.DTO;
+global using CoolSprings.API.Services;
+global using System.Net;
+global using Microsoft.AspNetCore.Http.HttpResults;
+global using CoolSprings.API.Filter;
+global using System.Globalization;
+global using System.IO;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc.Filters;
+global using Microsoft.Extensions.DependencyInjection;
+global using Newtonsoft.Json;
+global using Twilio;
+global using Twilio.Rest.Api.V2010.Account;
+global using Twilio.Types;
+
+
