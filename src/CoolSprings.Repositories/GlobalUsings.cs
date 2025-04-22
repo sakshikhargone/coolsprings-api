918d58f2003531193357c4fe3c60dfe4403d8c13
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-22 23:03:36 +0530

    Add GlobalUsings.cs for common repository namespace

---
+++ src/CoolSprings.Repositories/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using System.Runtime.InteropServices;
+global using System.Threading.Tasks;
+global using CoolSprings.Contract;
+global using CoolSprings.Model;
+global using Dapper;
+global using CoolSprings.DTO;
+global using Microsoft.Data.SqlClient;
+global using System.Collections.Generic;
+global using System.Data;
+global using System.Linq;
+global using System.Text;
+
