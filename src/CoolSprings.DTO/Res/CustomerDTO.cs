ebdee17640a8b9960ee9fccc17aa0e94c950a6d2
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-30 23:00:41 +0530

    Create CustomerDTO for shaping API response

---
+++ src/CoolSprings.DTO/Res/CustomerDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CoolSprings.DTO.Res;
+
+public  class CustomerDTO
+{
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsActive { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerPassword { get; set; }
+}
