0e376ad4a4ce3df6959d851b71f26edb49a4c6cc
sakshikhargone <sakshi.khargone@gmail.com>
2025-05-01 22:33:59 +0530

    Add ApiResponse DTO to standardize all API responses

---
+++ src/CoolSprings.DTO/Res/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace CoolSprings.DTO.Res;
+
+public class ApiResponse
+{
+    public ApiResponse(HttpStatusCode statusCode, bool success = false, object data = null, string message = "")
+    {
+        StatusCode = statusCode;
+        Success = success;
+        Data = data;
+        Message = message;
+    }
+
+    public HttpStatusCode StatusCode { get; set; }
+    public bool Success { get; set; }
+    public object Data { get; set; }
+    public string Message { get; set; }
+}
