bc853ff5ce31b95b7f990951b2a072293ef0703a
sakshikhargone <sakshi.khargone@gmail.com>
2025-05-22 23:06:14 +0530

    Add ICustomerRepository interface for customer data operations

---
+++ src/Coolsprings.Contract/Repository/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolSprings.Contract.Repository;
+
+public interface ICustomerRepository
+{
+    Task AddCustomer(Customer newCustomer);
+    Task<Customer> GetCustomer(string CustomerPhone);
+    Task UpdateCustomer(TokenCredential credential);
+    Task <CustomerHistoryDTO>GetCustomerHistory(Guid customerId);
+}
