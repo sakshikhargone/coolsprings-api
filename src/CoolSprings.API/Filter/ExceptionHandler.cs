1cbc3492f56c04f434b085b2c036efef5aa02ec8
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-01 13:23:46 +0530

    Add reusable 'ApiException' filter for consistent error handling

---
+++ src/CoolSprings.API/Filter/ExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+
+namespace CoolSprings.API.Filter
+{
+    public class ApiException : ExceptionFilterAttribute
+    {
+        private IExceptionRepository _exceptionRepo;
+
+        public override async Task OnExceptionAsync(ExceptionContext context)
+        {
+            _exceptionRepo = context.HttpContext.RequestServices.GetService<IExceptionRepository>();
+
+            if (_exceptionRepo != null)
+            {
+                var exceptionDetail = new ExceptionLog
+                {
+                    Id = Guid.NewGuid(),
+                    StackTrace = context.Exception.StackTrace,
+                    Message = context.Exception.Message,
+                    RequestUrl = context.HttpContext.Request.GetEncodedUrl(),
+                    CreatedAt = DateTime.Now,
+                };
+                await _exceptionRepo.AddException(exceptionDetail);
+            }
+
+            var encodedUrl = context.HttpContext.Request.GetEncodedUrl();
+            context.Result = new ObjectResult("An unexpected error occurred")
+            {
+                StatusCode = 500,
+                Value = Uri.UnescapeDataString(encodedUrl)
+            };
+
+            await base.OnExceptionAsync(context);
+        }
+    }
+}
