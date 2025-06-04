0452f07ea91e0237a4194d283859cfdd8e2835d2
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-04 16:05:22 +0530

    Add Exception to handle exception-related DB operations

---
+++ src/CoolSprings.Repositories/ExceptionRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace CoolSprings.Repository;
+
+public class ExceptionRepository : BaseRepository, IExceptionRepository
+{
+    public async Task AddException(ExceptionLog newException)
+    {
+        var sql = """
+                  INSERT INTO ExceptionLog(Id, StackTrace, Message, RequestUrl,CreatedAt)
+                  Values(@Id, @StackTrace, @Message, @RequestUrl,@CreatedAt)
+                  """;
+        using var db = Connect();
+        await db.ExecuteAsync(sql, new
+        {
+            Id = newException.Id,
+            StackTrace = newException.StackTrace,
+            Message = newException.Message,
+            RequestUrl = newException.RequestUrl,
+            CreatedAt = newException.CreatedAt
+        });
+    }
+}
