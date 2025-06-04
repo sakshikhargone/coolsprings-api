3743d4263ce6d7c99078fde727a489244b56eeaf
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-04 16:06:15 +0530

    Add Exception Model

---
+++ src/CoolSprings.Model/ExceptionLog.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolSprings.Model;
+
+public class ExceptionLog
+{
+    public Guid Id { set; get; }
+    public string StackTrace { get; set; }
+    public string Message { get; set; }
+    public string RequestUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
