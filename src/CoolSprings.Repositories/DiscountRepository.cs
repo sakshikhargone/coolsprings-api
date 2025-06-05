cee3400476ed8f0a40635d4adec485363e41cefb
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-05 21:07:58 +0530

    Implement 'DiscountRepository' to handle Discount-related DB operations

---
+++ src/CoolSprings.Repositories/DiscountRepository.cs
@@ -0,0 +1,53 @@
+﻿using CoolSprings.Contract.Repository;
+
+namespace CoolSprings.Repository;
+
+public class DiscountRepository : BaseRepository, IDiscountRepository
+{
+    public async Task AddDiscount(Discount newDiscount)
+    {
+        try
+        {
+            var sql = """
+                      INSERT INTO Discount(Id,Code,DiscountValue,ExpiryDate,ActivationDate)
+                      VALUES(@Id,@Code,@DiscountValue,@ExpiryDate,@ActivationDate)
+                      """;
+            using var db = Connect();
+            await db.ExecuteAsync(sql, new
+            {
+                Id = newDiscount.DiscountId,
+                Code = newDiscount.DiscountCode,
+                DiscountValue = 10,
+                ExpiryDate = DateTime.Now.AddDays(5),
+                ActivationDate = DateTime.Now
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+    public async Task<Discount> GetDiscount(string discountCode)
+    {
+        try
+        {
+            var sql = """
+                      SELECT * FROM Discount
+                      WHERE Code = @discountCode
+                      """;
+            using var db = Connect();
+            var discount = await db.QueryFirstOrDefaultAsync<Discount>(sql, new
+            {
+                discountCode
+            });
+            return discount;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
