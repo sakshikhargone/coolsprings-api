e67d9b758e1943e8b85eb9584ea23354adf4aa95
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-08 16:55:22 +0530

    Implement 'TokenRepository' to handle token-related DB operations

---
+++ src/CoolSprings.Repositories/TokenRepository.cs
@@ -0,0 +1,38 @@
+﻿using CoolSprings.Contract.Repository;
+
+namespace CoolSprings.Repository;
+
+public class TokenRepository : BaseRepository, ITokenRepository
+{
+    public async Task AddToken(Token newToken)
+    {
+        var sql = """
+                  INSERT INTO Token(TokenId, TokenValue,CustomerId,ValidFrom, ExpireAt, IsActive)
+                  VALUES(@TokenId, @TokenValue, @CustomerId, @ValidFrom, @ExpireAt, @IsActive)
+                  """;
+        using var db = Connect();
+        await db.ExecuteAsync(sql, new
+        {
+            TokenId = newToken.TokenId,
+            TokenValue = newToken.TokenValue,
+            CustomerId = newToken.CustomerId,
+            ValidFrom = newToken.ValidFrom,
+            ExpireAt = newToken.ExpireAt,
+            IsActive = newToken.IsActive
+        }, commandTimeout: commandTimeoutSeconds);
+    }
+
+    public async Task<Token> GetToken(string customerPhone)
+    {
+        var sql = """
+                  SELECT * FROM Token t
+                  JOIN Customer c ON t.CustomerId = c.CustomerId
+                  WHERE c.CustomerPhone = @customerPhone AND
+                  t.ExpireAt > GETDATE()
+                  """;
+        using var db = Connect();
+        var token = await db.QueryFirstOrDefaultAsync<Token>(sql, new { customerPhone },
+            commandTimeout: commandTimeoutSeconds);
+        return token;
+    }
+}
