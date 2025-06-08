8a656cb8c5c39bbe58c9c78c00b82a780f3d08bb
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-08 16:54:28 +0530

    Add Token model

---
+++ src/CoolSprings.Model/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace CoolSprings.Model;
+
+public class Token
+{
+    public Guid TokenId { get; set; }
+    public string TokenValue { get; set; }
+    public Guid CustomerId { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ExpireAt { get; set; }
+    public bool IsActive { get; set; }
+}
--- src/Coolsprings.Contract/Repository/ITokenRepository.cs
+++ src/Coolsprings.Contract/Repository/ITokenRepository.cs
@@ -3,5 +3,6 @@
 public interface ITokenRepository
 {
     Task AddToken(Token newToken);
+
     Task<Token> GetToken(string customerPhone);
 }
