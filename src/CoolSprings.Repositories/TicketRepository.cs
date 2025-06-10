a5dc9ca699ec1584c44cd6acf7f55d812fa00ea3
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-10 16:41:31 +0530

    Implement 'TicketRepository' to handle ticket-related DB operations

---
+++ src/CoolSprings.Repositories/TicketRepository.cs
@@ -0,0 +1,59 @@
+﻿using CoolSprings.Contract.Repository;
+using CoolSprings.DTO.Res;
+
+namespace CoolSprings.Repository;
+
+public class TicketRepository : BaseRepository, ITicketRepository
+{
+    public async Task AddTicket(Ticket newTicket)
+    {
+        try
+        {
+            var sql = """
+                     INSERT INTO Ticket(TicketId,BookingId,IsValid)
+                     VALUES(@TicketId, @BookingId, @IsValid)
+                     """;
+            using var db = Connect();
+            await db.ExecuteAsync(sql, new
+            {
+                TicketId = newTicket.TicketId,
+                BookingId = newTicket.BookingId,
+                IsValid = newTicket.IsValid
+            }, commandTimeout: commandTimeoutSeconds);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+    public async Task<TicketDetail> GetTicket(Guid bookingId)
+    {
+        try
+        {
+            var tickets = """
+                          SELECT TicketId FROM Ticket
+                          WHERE BookingId = @bookingId
+                          """;
+            var bookingDate = """
+                              SELECT ValidFrom,ExpiryDate FROM Booking
+                              WHERE BookingId = @bookingId
+                              """;
+            using var db = Connect();
+            var ticketDetail = await db.QueryFirstOrDefaultAsync<TicketDetail>(bookingDate,
+                new { bookingId }, commandTimeout: commandTimeoutSeconds);
+            if (ticketDetail != null)
+            {
+                var ticket = await db.QueryAsync<DetailOfTicket>(tickets, new { bookingId });
+                ticketDetail.TicketId = ticket;
+            }
+            return ticketDetail;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
