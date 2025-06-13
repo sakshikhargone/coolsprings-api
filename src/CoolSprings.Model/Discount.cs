fd6c0a8115ae8cbbf5a3e70b4f2c6f5f18b6ccc9
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-13 22:08:11 +0530

    Add Customer, Discount model

---
+++ src/CoolSprings.Model/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolSprings.Model;
+
+public class Discount
+{
+    public Guid DiscountId { get; set; }
+    public string DiscountCode { get; set; }
+    public string DiscountValue { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public DateTime ActivationDate { get; set; }
+}
