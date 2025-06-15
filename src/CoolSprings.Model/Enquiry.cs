c61b756038ca5aea940172cb77a6eb011ec3120c
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-15 13:52:50 +0530

    Add Enquiry model

---
+++ src/CoolSprings.Model/Enquiry.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CoolSprings.Model;
+
+public class Enquiry
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+    public string ContactNo { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
