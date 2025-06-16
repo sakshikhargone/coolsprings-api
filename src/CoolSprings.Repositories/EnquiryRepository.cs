f1659a28654c6b3f05a44640317fc8a7f7d575c6
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-16 20:05:05 +0530

    Implement 'EnquiryRepository' to handle enquiry-related DB operations

---
+++ src/CoolSprings.Repositories/EnquiryRepository.cs
@@ -0,0 +1,25 @@
+﻿
+
+using CoolSprings.Contract.Repository;
+
+namespace CoolSprings.Repository;
+
+public class EnquiryRepository:BaseRepository,IEnquiryRepository
+{
+    public async Task AddEnquiry(Enquiry newEnquiry)
+    {
+        var sql = """
+                   INSERT INTO Enquiry( Email, Message, CreatedAt,ContactNo)
+                  VALUES( @Email, @Message, @CreatedAt,@ContactNo )
+                  """;
+        using var db = Connect();
+        await db.ExecuteAsync(sql, new
+        {
+            
+            Email = newEnquiry.Email,
+            Message = newEnquiry.Message,
+            CreatedAt = newEnquiry.CreatedAt,
+            ContactNo = newEnquiry.ContactNo
+        });
+    }
+}
