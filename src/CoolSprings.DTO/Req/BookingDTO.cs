6b4637f5a80f7315e4eed3368e8be5432bef30cf
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-20 19:42:32 +0530

    Add 'BookingDTO' class for Booking DTO

---
+++ src/CoolSprings.DTO/Req/BookingDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CoolSprings.DTO.Req;
+
+public class BookingDTO
+{
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public string? DiscountCode { get; set; }
+    public int NumOfTickets { get; set; }
+    public string PaymentMode { get; set; }
+    public float ActualAmount { get; set; }
+    public float PaidAmount { get; set; }
+    public string? UPITxnRefNumber { get; set; }
+}
