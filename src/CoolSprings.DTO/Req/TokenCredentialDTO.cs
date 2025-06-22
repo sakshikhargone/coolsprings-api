e6eae3765ce22ee2d68027808480e9eacae6c1b0
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-22 23:52:54 +0530

    Add TokenCredentialDTO

---
+++ src/CoolSprings.DTO/Req/TokenCredentialDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CoolSprings.DTO;
+
+public class TokenCredentialDTO
+{
+    public string CustomerPhone { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerPassword { get; set; }
+    public Guid CustomerId { get; set; }
+}
