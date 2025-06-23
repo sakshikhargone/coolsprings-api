40366fa764e89281efd7ea2d14637903dc10d556
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-23 23:51:12 +0530

    Add BookingDetailDTO for booking detail response

---
+++ src/CoolSprings.DTO/Res/BookingDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CoolSprings.DTO.Res;
+
+public  class BookingDetailDTO
+{
+    public string ValidFrom { get; set; }
+    public string ExpiryDate { get; set; }
+    public IEnumerable<DetailOfTicket> TicketId { get; set; }
+}
+public class DetailOfTicket
+{
+    public Guid TicketId { get; set; }
+}
