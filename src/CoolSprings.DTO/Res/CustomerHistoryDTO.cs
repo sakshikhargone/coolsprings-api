2272cf4d09b3e95b1464fbe67019e90aed21084b
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-24 23:45:29 +0530

    Add CustomerHistoryDTO for booking history response

---
+++ src/CoolSprings.DTO/Res/CustomerHistoryDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace CoolSprings.DTO;
+
+public class CustomerHistoryDTO
+{
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public IEnumerable<BookingDetail> Booking { get; set; }
+}
+public  class BookingDetail
+{
+    public int NumOfTickets { get; set; }
+    public string PaymentMode { get; set; }
+    public float ActualAmount { get; set; }
+    public float PaidAmount { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ExpiryDate { get; set; }
+}
