fd3980023f3519f6e15d7b93040866b1c2d0de2e
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-25 23:54:25 +0530

    Add API route constants for core controllers

---
+++ src/CoolSprings.DTO/ApiEndpoint.cs
@@ -0,0 +1,31 @@
+﻿namespace CoolSprings.DTO;
+
+public static class ApiEndpoint
+{
+    public const string ApiBase = "api/v1";
+
+    public static class Booking
+    {
+        public const string BookingBase = ApiBase + "/bookings";
+    }
+
+    public static class Ticket
+    {
+        public const string TicketBase = ApiBase + "/tickets";
+    }
+
+    public static class Customer
+    {
+        public const string CustomerBase = ApiBase + "/customers";
+    }
+
+    public static class Discount
+    {
+        public const string DiscountBase = ApiBase + "/discounts";
+    }
+
+    public static class Enquiry
+    {
+        public const string EnquiryBase = ApiBase + "/enquiries";
+    }
+}
