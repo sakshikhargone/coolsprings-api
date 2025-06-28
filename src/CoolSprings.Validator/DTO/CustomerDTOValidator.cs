acaa14667ec334fe15c61245703435e3fdd5441f
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-28 23:54:57 +0530

    Add CustomerDtoValidator for validating CustomerDto

---
+++ src/CoolSprings.Validator/DTO/CustomerDTOValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace CoolSprings.Validator.DTO;
+
+public class CustomerDTOValidator : AbstractValidator<CustomerDTO>
+{
+    public CustomerDTOValidator()
+    {
+        RuleFor(Customer => Customer.CustomerId)
+        .NotEmpty().WithMessage("CustomerId is required");
+
+        RuleFor(Customer => Customer.CustomerName)
+       .NotEmpty().WithMessage("CustomerName is required");
+
+        RuleFor(Customer => Customer.CustomerPhone)
+       .NotEmpty().WithMessage("CustomerPhone is required");
+
+        RuleFor(Customer => Customer.CreatedAt)
+      .NotEmpty().WithMessage("CreatedAt is required");
+
+        RuleFor(Customer => Customer.CustomerEmail)
+      .NotEmpty().WithMessage("CustomerEmail is required");
+
+        RuleFor(Customer => Customer.CustomerPassword)
+     .NotEmpty().WithMessage("CustomerPassword is required");
+    }
+}
