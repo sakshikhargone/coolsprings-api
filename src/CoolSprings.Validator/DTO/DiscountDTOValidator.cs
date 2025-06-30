078924cfac72c8816d08f5aa526f57c24f0fda4f
sakshikhargone <sakshi.khargone@gmail.com>
2025-06-30 23:52:42 +0530

    Create DiscountDtoValidator for validating discount data

---
+++ src/CoolSprings.Validator/DTO/DiscountDTOValidator.cs
@@ -0,0 +1,28 @@
+﻿namespace CoolSprings.Validator.DTO;
+
+    public class DiscountDTOValidator : AbstractValidator<DiscountDTO>
+    {
+        public DiscountDTOValidator()
+        {
+            RuleFor(x => x.DiscountId)
+                .NotEmpty().WithMessage("Discount ID is required.");
+
+            RuleFor(x => x.DiscountCode)
+                .NotEmpty().WithMessage("Discount code is required.")
+                .MaximumLength(25).WithMessage("Discount code cannot exceed 25 characters.");
+
+        RuleFor(x => x.DiscountValue)
+            .NotEmpty().WithMessage("Discount value is required.");
+              
+
+        RuleFor(x => x.ActivationDate)
+                .LessThanOrEqualTo(x => x.ExpiryDate)
+                .WithMessage("Activation date must be before or equal to the expiry date.");
+
+            RuleFor(x => x.ExpiryDate)
+                .GreaterThanOrEqualTo(DateTime.Today)
+                .WithMessage("Expiry date should not be less than today's date");
+        }
+    }
+
+
