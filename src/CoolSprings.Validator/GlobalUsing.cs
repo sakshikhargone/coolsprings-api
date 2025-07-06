b2fe6eb55aa1175587084b9c9bdb065025999345
sakshikhargone <sakshi.khargone@gmail.com>
2025-07-06 23:41:57 +0530

    Add global using file for Coolspring validator

---
+++ src/CoolSprings.Validator/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using CoolSprings.DTO;
+global using FluentValidation;
+global using CoolSprings.Model;
+global using CoolSprings.DTO.Res;
+global using CoolSprings.DTO.Req;
